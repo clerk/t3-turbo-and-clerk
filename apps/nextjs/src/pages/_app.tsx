e57d1c574bc08c5912a5a78789c8ba791dfb4404
Cozy Indigo <berkutkarlibai@gmail.com>
2023-04-04 13:39:13 +0600

    Added dotenv for ClerkProvider

--- apps/nextjs/src/pages/_app.tsx
+++ apps/nextjs/src/pages/_app.tsx
@@ -3,10 +3,14 @@ import "../styles/globals.css";
 import type { AppType } from "next/app";
 import { ClerkProvider } from "@clerk/nextjs";
 import { trpc } from "../utils/trpc";
+import { clientEnv } from "../env/schema.mjs";
 
 const MyApp: AppType = ({ Component, pageProps: { ...pageProps } }) => {
   return (
-    <ClerkProvider {...pageProps}>
+    <ClerkProvider
+      {...pageProps}
+      publishableKey={clientEnv.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+    >
       <Component {...pageProps} />
     </ClerkProvider>
   );
