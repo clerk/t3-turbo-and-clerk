5363ad7593de025b1dbc53f6ff7f0d8879a393f5
James P <jamesperkins@hey.com>
2023-04-07 10:05:46 -0400

    Update useWarmUpBrowser.ts

--- apps/expo/src/hooks/useWarmUpBrowser.ts
+++ apps/expo/src/hooks/useWarmUpBrowser.ts
@@ -1,7 +1,7 @@
 import React from "react";
 import * as WebBrowser from "expo-web-browser";
 
-export const useWamUpBrowser = () => {
+export const useWarmUpBrowser = () => {
   React.useEffect(() => {
     void WebBrowser.warmUpAsync();
     return () => {
