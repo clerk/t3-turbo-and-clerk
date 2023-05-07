8e81b92fc9ae162a21897f9df821ff146545450a
Spastic <junior.tan@live.com>
2023-05-07 18:57:24 +0800

    env

--- apps/nextjs/src/env/schema.mjs
+++ apps/nextjs/src/env/schema.mjs
@@ -17,6 +17,7 @@ export const serverSchema = z.object({
  */
 export const clientSchema = z.object({
   NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string().optional(),
+  NEXT_PUBLIC_CLERK_WEBHOOK_SECRET: z.string(),
 });
 
 /**
@@ -26,5 +27,8 @@ export const clientSchema = z.object({
  * @type {{ [k in keyof z.infer<typeof clientSchema>]: z.infer<typeof clientSchema>[k] | undefined }}
  */
 export const clientEnv = {
-  NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+  NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
+    process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+  NEXT_PUBLIC_CLERK_WEBHOOK_SECRET:
+    process.env.NEXT_PUBLIC_CLERK_WEBHOOK_SECRET,
 };
