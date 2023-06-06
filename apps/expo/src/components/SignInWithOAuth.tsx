191373159765712c3913f9916502c752fe2cf1a2
DontAskForAnything <misiujasina@gmail.com>
2023-06-06 21:15:17 +0200

    fixed styling

--- apps/expo/src/components/SignInWithOAuth.tsx
+++ apps/expo/src/components/SignInWithOAuth.tsx
@@ -16,16 +16,14 @@ const SignInWithOAuth = () => {
         setActive?.({ session: createdSessionId });
       } else {
         // Modify this code to use signIn or signUp to set this missing requirements you set in your dashboard.
-        throw new Error(
-          "There are unmet requirements, modifiy this else to handle them",
-        );
+        throw new Error("There are unmet requirements, modifiy this else to handle them")
+
       }
     } catch (err) {
       console.log(JSON.stringify(err, null, 2));
       console.log("error signing in", err);
     }
   }, []);
-
   return (
     <View className="rounded-lg border-2 border-gray-500 p-4">
       <Button
@@ -34,6 +32,6 @@ const SignInWithOAuth = () => {
       />
     </View>
   );
-};
+}
 
 export default SignInWithOAuth;
