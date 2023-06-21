96ef9fa04ef170269f76e618b7c04db0b1dc5abf
Afonso Jorge Ramos <afonsojorgeramos@gmail.com>
2023-06-21 11:00:36 +0100

    chore: update `@clerk/nextjs`

--- apps/nextjs/src/_middleware.ts
+++ apps/nextjs/src/_middleware.ts
@@ -1,21 +1,7 @@
-import { NextResponse, type NextRequest } from "next/server";
-import { withClerkMiddleware } from "@clerk/nextjs/server";
+import { authMiddleware } from "@clerk/nextjs";
 
-export default withClerkMiddleware((_req: NextRequest) => {
-  return NextResponse.next();
-});
+export default authMiddleware();
 
-// Stop Middleware running on static files
 export const config = {
-  matcher: [
-    /*
-     * Match request paths except for the ones starting with:
-     * - _next
-     * - static (static files)
-     * - favicon.ico (favicon file)
-     *
-     * This includes images, and requests from TRPC.
-     */
-    "/(.*?trpc.*?|(?!static|.*\\..*|_next|favicon.ico).*)",
-  ],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
